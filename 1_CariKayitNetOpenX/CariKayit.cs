d3edb619269e056414a23b2c94478b06a6808651
Murat Acun <83479146+Acun9@users.noreply.github.com>
2024-05-23 17:32:16 +0300

    update scripts

--- 1_CariKayitNetOpenX/CariKayit.cs
+++ 1_CariKayitNetOpenX/CariKayit.cs
@@ -16,113 +16,115 @@ namespace SinerjiPanel
 {
     public partial class CariKayit : Form
     {
-        private Kernel kernel;
-        private Sirket sourceSirket;
-        private Sirket destinationSirket;
-
         public CariKayit()
         {
-            InitializeComponent();
+            InitializeComponent();            
         }
 
         private void CariKayit_Load(object sender, EventArgs e)
         {
-            LoadComboboxData();            
+            LoadComboboxData();
         }
 
-        private void InitializeNetOpenX()
+        private void LoadComboboxData()
         {
-            kernel = new Kernel();
-            sourceSirket = kernel.yeniSirket(TVTTipi.vtMSSQL, "SINERJICRM", "sa", "SA123pass_", "netsis", "5091", 0);
-            destinationSirket = kernel.yeniSirket(TVTTipi.vtMSSQL, "SINERJISMART", "TemelSet", "", "netsis", "5091", 0);
+            LoadUlkeler();
+            LoadIller();
+            LoadGrupKodlari();
+            LoadRaporKodlari();
+            LoadPostaKodlari();
         }
 
-        private void btnAktar_Click(object sender, EventArgs e)
+        private void LoadUlkeler()
         {
-            InitializeNetOpenX();
-            try
+            cmbUlke.Items.Add("TR");
+            cmbUlke.Items.Add("EN");
+        }
+
+        private void LoadIller()
+        {
+            using (SqlConnection connection = new SQLBaglantisi().baglanti())
             {
-                string selectQuery = "SELECT * FROM CARI_KAYIT";
-                using (SqlConnection sourceConnection = new SqlConnection("Server=SRV1;Database=SINERJICRM;User Id=sa;Password=SA123pass_;"))
+                string query = "SELECT id, sehiradi FROM iller";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
                 {
-                    sourceConnection.Open();
-                    SqlCommand selectCommand = new SqlCommand(selectQuery, sourceConnection);
-                    using (SqlDataReader reader = selectCommand.ExecuteReader())
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
                     {
-                        while (reader.Read())
-                        {
-                            ProcessRecord(reader);
-                        }
+                        cmbIl.Items.Add(reader["sehiradi"].ToString());
+                    }
+                }
+            }
+        }
+
+        private void LoadIlceler(int sehirId)
+        {
+            cmbIlce.Items.Clear();
+
+            using (SqlConnection connection = new SQLBaglantisi().baglanti())
+            {
+                string query = "SELECT id, ilceadi FROM ilceler WHERE sehirid = @sehirid";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@sehirid", sehirId);
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        cmbIlce.Items.Add(reader["ilceadi"].ToString());
+                    }
+                }
+            }
+        }
+
+        private void LoadGrupKodlari()
+        {
+            // Grup kodlarını yükleyecek kod
+            cmbGrupKodu.Items.Add("grpkod");
+        }
+
+        private void LoadRaporKodlari()
+        {
+            // Rapor kodlarını yükleyecek kod
+            cmbRaporKodu1.Items.Add("rprkod1");
+            cmbRaporKodu2.Items.Add("rprkod2");
+        }
+
+        private void LoadPostaKodlari()
+        {
+            cmbPostaKodu.Items.Add("pstkod");
+        }
+
+        private void cmbIl_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbIl.SelectedItem != null)
+            {
+                string sehirAdi = cmbIl.SelectedItem.ToString();
+                int sehirId;
+
+                using (SqlConnection connection = new SQLBaglantisi().baglanti())
+                {
+                    string query = "SELECT id FROM iller WHERE sehiradi = @sehiradi";
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@sehiradi", sehirAdi);
+                        object result = command.ExecuteScalar();
+                        sehirId = result != null ? (int)result : -1;
                     }
                 }
 
-                DeleteRemovedRecords();
-                MessageBox.Show("Veri aktarımı başarıyla tamamlandı.");
+                if (sehirId != -1)
+                {
+                    LoadIlceler(sehirId);
+                }
+                else
+                {
+                    cmbIlce.Items.Clear();
+                }
             }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Veri aktarımı sırasında hata oluştu: {ex.Message}");
-            }
-            finally
-            {
-                CleanupNetOpenX();
-            }
-        }
-
-        private void ProcessRecord(SqlDataReader reader)
-        {
-            // NetOpenX kullanarak veri ekleme/güncelleme mantığı
-            Cari cari = kernel.yeniCari(destinationSirket);
-            CariTemelBilgi cariTmlBlg = cari.TemelBilgi();
-            CariEkBilgi cariEkBlg = cari.EkBilgi();
-
-
-
-
-            // Veritabanı alanlarını Cari nesnesine ayarla
-            cariTmlBlg.CARI_KOD = reader["CARI_KOD"].ToString();
-            cariTmlBlg.CARI_ISIM = reader["CARI_ISIM"].ToString();
-            cariTmlBlg.CARI_ADRES = reader["ADRES"].ToString();
-            cariTmlBlg.ULKE_KODU = reader["ULKE"].ToString();
-            cariTmlBlg.CARI_IL = reader["IL"].ToString();
-            cariTmlBlg.CARI_ILCE = reader["ILCE"].ToString();
-            cariTmlBlg.POSTAKODU = reader["POSTA_KODU"].ToString();
-            cariTmlBlg.CARI_TEL = reader["TEL"].ToString();
-            cariTmlBlg.FAX = reader["FAKS"].ToString();
-            cariTmlBlg.EMAIL = reader["MAIL"].ToString();
-            cariTmlBlg.WEB = reader["WEB"].ToString();
-            cariTmlBlg.VERGI_DAIRESI = reader["VERGI_DAIRE"].ToString();
-            cariTmlBlg.VERGI_NUMARASI = reader["VERGI_NO"].ToString();
-            cariTmlBlg.Grup_Kodu = reader["GRUP_KODU"].ToString();
-            cariTmlBlg.RAPOR_KODU1 = reader["RAPOR_KODU_1"].ToString();
-            cariTmlBlg.Rapor_Kodu2 = reader["RAPOR_KODU_2"].ToString();
-            cariTmlBlg.Sube_Kodu = 0; // Varsayılan değer
-            cariTmlBlg.ISLETME_KODU = 1; // Varsayılan değer
-
-            // Veritabanına kaydet
-            cari.kayitYeni();
-        }
-
-        private void DeleteRemovedRecords()
-        {
-            // NetOpenX kullanarak kayıtları silme mantığı
-            using (SqlConnection destinationConnection = new SqlConnection("Server=SRV1;Database=SINERJISMART;User Id=netsis;Password=5091;"))
-            {
-                destinationConnection.Open();
-                string deleteQuery = @"
-                DELETE FROM TBLCASABIT
-                WHERE CARI_KOD NOT IN (SELECT CARI_KOD FROM [SINERJICRM].[dbo].[CARI_KAYIT])";
-                SqlCommand deleteCommand = new SqlCommand(deleteQuery, destinationConnection);
-                deleteCommand.ExecuteNonQuery();
-            }
-        }
-
-        private void CleanupNetOpenX()
-        {
-            Marshal.ReleaseComObject(destinationSirket);
-            Marshal.ReleaseComObject(sourceSirket);
-            kernel.FreeNetsisLibrary();
-            Marshal.ReleaseComObject(kernel);
         }
 
         private void btnEkle_Click(object sender, EventArgs e)
@@ -299,104 +301,80 @@ namespace SinerjiPanel
             this.Close();
         }
 
-        private void LoadComboboxData()
-        {
-            LoadUlkeler();
-            LoadIller();
-            LoadGrupKodlari();
-            LoadRaporKodlari();
-            LoadPostaKodlari();
-        }
+        /* ========================================================================= [ NetOpenX ] ========================================================================= */
 
-        private void LoadUlkeler()
+        private void btnAktar_Click(object sender, EventArgs e)
         {
-            cmbUlke.Items.Add("TR");
-            cmbUlke.Items.Add("EN");
-        }
 
-        private void LoadIller()
-        {
-            using (SqlConnection connection = new SQLBaglantisi().baglanti())
+            // NetOpenX için şirket tanımlama
+            Kernel kernel = new Kernel();
+            Sirket sourceSirket = kernel.yeniSirket(TVTTipi.vtMSSQL, "SINERJICRM", "sa", "SA123pass_", "netsis", "5091", 0);
+            Sirket destinationSirket = kernel.yeniSirket(TVTTipi.vtMSSQL, "SINERJISMART", "TEMELSET", "", "netsis", "5091", 0);
+            Cari cari = kernel.yeniCari(destinationSirket);
+            CariTemelBilgi cariTmlBlg = cari.TemelBilgi();
+            //CariEkBilgi cariEkBlg = cari.EkBilgi();
+
+            try
             {
-                string query = "SELECT id, sehiradi FROM iller";
-
-                using (SqlCommand command = new SqlCommand(query, connection))
+                string selectQuery = "SELECT * FROM CARI_KAYIT";
+                using (SqlConnection sourceConnection = new SqlConnection("Server=SRV1;Database=SINERJICRM;User Id=sa;Password=SA123pass_;"))
                 {
-                    SqlDataReader reader = command.ExecuteReader();
-
-                    while (reader.Read())
+                    sourceConnection.Open();
+                    SqlCommand selectCommand = new SqlCommand(selectQuery, sourceConnection);
+                    using (SqlDataReader reader = selectCommand.ExecuteReader())
                     {
-                        cmbIl.Items.Add(reader["sehiradi"].ToString());
+                        // NetOpenX kullanarak veri ekleme/güncelleme mantığı                        
+                        cariTmlBlg.CARI_KOD = reader["CARI_KOD"].ToString();
+                        cariTmlBlg.CARI_ISIM = reader["CARI_ISIM"].ToString();
+                        cariTmlBlg.CARI_ADRES = reader["ADRES"].ToString();
+                        cariTmlBlg.ULKE_KODU = reader["ULKE"].ToString();
+                        cariTmlBlg.CARI_IL = reader["IL"].ToString();
+                        cariTmlBlg.CARI_ILCE = reader["ILCE"].ToString();
+                        cariTmlBlg.POSTAKODU = reader["POSTA_KODU"].ToString();
+                        cariTmlBlg.CARI_TEL = reader["TEL"].ToString();
+                        cariTmlBlg.FAX = reader["FAKS"].ToString();
+                        cariTmlBlg.EMAIL = reader["MAIL"].ToString();
+                        cariTmlBlg.WEB = reader["WEB"].ToString();
+                        cariTmlBlg.VERGI_DAIRESI = reader["VERGI_DAIRE"].ToString();
+                        cariTmlBlg.VERGI_NUMARASI = reader["VERGI_NO"].ToString();
+                        cariTmlBlg.Grup_Kodu = reader["GRUP_KODU"].ToString();
+                        cariTmlBlg.RAPOR_KODU1 = reader["RAPOR_KODU_1"].ToString();
+                        cariTmlBlg.Rapor_Kodu2 = reader["RAPOR_KODU_2"].ToString();
+                        cariTmlBlg.Sube_Kodu = 0; // Varsayılan değer
+                        cariTmlBlg.ISLETME_KODU = 1; // Varsayılan değer
+
+                        // Veritabanına kaydet
+                        cari.kayitYeni();
                     }
                 }
+
+                // NetOpenX kullanarak kayıtları silme mantığı
+                using (SqlConnection destinationConnection = new SqlConnection("Server=SRV1;Database=SINERJISMART;User Id=netsis;Password=5091;"))
+                {
+                    destinationConnection.Open();
+                    string deleteQuery = @"
+                DELETE FROM TBLCASABIT
+                WHERE CARI_KOD NOT IN (SELECT CARI_KOD FROM [SINERJICRM].[dbo].[CARI_KAYIT])";
+                    SqlCommand deleteCommand = new SqlCommand(deleteQuery, destinationConnection);
+                    deleteCommand.ExecuteNonQuery();
+                }
+
+                MessageBox.Show("Veri aktarımı başarıyla tamamlandı.");
             }
-        }
-
-        private void LoadIlceler(int sehirId)
-        {
-            cmbIlce.Items.Clear();
-
-            using (SqlConnection connection = new SQLBaglantisi().baglanti())
+            catch (Exception ex)
             {
-                string query = "SELECT id, ilceadi FROM ilceler WHERE sehirid = @sehirid";
-
-                using (SqlCommand command = new SqlCommand(query, connection))
-                {
-                    command.Parameters.AddWithValue("@sehirid", sehirId);
-                    SqlDataReader reader = command.ExecuteReader();
-
-                    while (reader.Read())
-                    {
-                        cmbIlce.Items.Add(reader["ilceadi"].ToString());
-                    }
-                }
+                MessageBox.Show($"Veri aktarımı sırasında hata oluştu: {ex.Message}");
             }
-        }
-
-        private void LoadGrupKodlari()
-        {
-            // Grup kodlarını yükleyecek kod
-            cmbGrupKodu.Items.Add("grpkod");
-        }
-
-        private void LoadRaporKodlari()
-        {
-            // Rapor kodlarını yükleyecek kod
-            cmbRaporKodu1.Items.Add("rprkod1");
-            cmbRaporKodu2.Items.Add("rprkod2");
-        }
-
-        private void LoadPostaKodlari()
-        {
-            cmbPostaKodu.Items.Add("pstkod");
-        }
-
-        private void cmbIl_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            if (cmbIl.SelectedItem != null)
+            finally
             {
-                string sehirAdi = cmbIl.SelectedItem.ToString();
-                int sehirId;
-
-                using (SqlConnection connection = new SQLBaglantisi().baglanti())
-                {
-                    string query = "SELECT id FROM iller WHERE sehiradi = @sehiradi";
-                    using (SqlCommand command = new SqlCommand(query, connection))
-                    {
-                        command.Parameters.AddWithValue("@sehiradi", sehirAdi);
-                        object result = command.ExecuteScalar();
-                        sehirId = result != null ? (int)result : -1;
-                    }
-                }
-
-                if (sehirId != -1)
-                {
-                    LoadIlceler(sehirId);
-                }
-                else
-                {
-                    cmbIlce.Items.Clear();
-                }
+                // NetOpenX belleğini temizle (tanımladığımız nesneleri sondan başa doğru temizleriz)
+                //Marshal.ReleaseComObject(cariEkBlg);
+                Marshal.ReleaseComObject(cariTmlBlg);
+                Marshal.ReleaseComObject(cari);
+                Marshal.ReleaseComObject(destinationSirket);
+                Marshal.ReleaseComObject(sourceSirket);
+                kernel.FreeNetsisLibrary();
+                Marshal.ReleaseComObject(kernel);
             }
         }
     }
--- 2_CariKayitNetOpenXREST/obj/Debug/net8.0-windows/2_CariKayitNetOpenXREST.AssemblyInfo.cs
+++ 2_CariKayitNetOpenXREST/obj/Debug/net8.0-windows/2_CariKayitNetOpenXREST.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("2_CariKayitNetOpenXREST")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44d0a4967844a94266e72c74233381185390aa0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("2_CariKayitNetOpenXREST")]
 [assembly: System.Reflection.AssemblyTitleAttribute("2_CariKayitNetOpenXREST")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
