e29967a405420bbc85982dfbbb9069759342c31d
Murat Acun <83479146+Acun9@users.noreply.github.com>
2024-05-27 09:14:58 +0300

    fixes

---
+++ PlasiyerKayitPanel/MusteriKayit.Designer.cs
@@ -0,0 +1,432 @@
+﻿namespace C_Forms
+{
+    partial class MusteriKayit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnCikis = new Button();
+            btnKaydet = new Button();
+            cmbUlke = new ComboBox();
+            cmbIlce = new ComboBox();
+            cmbIl = new ComboBox();
+            label14 = new Label();
+            rtxtAciklama = new RichTextBox();
+            label13 = new Label();
+            label12 = new Label();
+            label11 = new Label();
+            txtAdres = new TextBox();
+            label10 = new Label();
+            txtTelefon = new TextBox();
+            label9 = new Label();
+            txtMail = new TextBox();
+            label8 = new Label();
+            txtYonetici = new TextBox();
+            label7 = new Label();
+            txtDepartman = new TextBox();
+            label6 = new Label();
+            txtGorev = new TextBox();
+            label5 = new Label();
+            txtAdSoyad = new TextBox();
+            label4 = new Label();
+            label3 = new Label();
+            txtMeslek = new TextBox();
+            label2 = new Label();
+            txtPlasiyerKodu = new TextBox();
+            label1 = new Label();
+            rbKadin = new RadioButton();
+            rbErkek = new RadioButton();
+            SuspendLayout();
+            // 
+            // btnCikis
+            // 
+            btnCikis.Location = new Point(681, 359);
+            btnCikis.Name = "btnCikis";
+            btnCikis.Size = new Size(75, 23);
+            btnCikis.TabIndex = 63;
+            btnCikis.Text = "Çıkış";
+            btnCikis.UseVisualStyleBackColor = true;
+            btnCikis.Click += btnCikis_Click;
+            // 
+            // btnKaydet
+            // 
+            btnKaydet.Location = new Point(518, 359);
+            btnKaydet.Name = "btnKaydet";
+            btnKaydet.Size = new Size(75, 23);
+            btnKaydet.TabIndex = 62;
+            btnKaydet.Text = "Kaydet";
+            btnKaydet.UseVisualStyleBackColor = true;
+            // 
+            // cmbUlke
+            // 
+            cmbUlke.Font = new Font("Segoe UI", 9F);
+            cmbUlke.FormattingEnabled = true;
+            cmbUlke.Location = new Point(154, 359);
+            cmbUlke.Name = "cmbUlke";
+            cmbUlke.Size = new Size(258, 23);
+            cmbUlke.TabIndex = 60;
+            // 
+            // cmbIlce
+            // 
+            cmbIlce.Font = new Font("Segoe UI", 9F);
+            cmbIlce.FormattingEnabled = true;
+            cmbIlce.Location = new Point(154, 330);
+            cmbIlce.Name = "cmbIlce";
+            cmbIlce.Size = new Size(258, 23);
+            cmbIlce.TabIndex = 59;
+            // 
+            // cmbIl
+            // 
+            cmbIl.Font = new Font("Segoe UI", 9F);
+            cmbIl.FormattingEnabled = true;
+            cmbIl.Location = new Point(154, 301);
+            cmbIl.Name = "cmbIl";
+            cmbIl.Size = new Size(258, 23);
+            cmbIl.TabIndex = 58;
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label14.Location = new Point(609, 184);
+            label14.Name = "label14";
+            label14.Size = new Size(60, 15);
+            label14.TabIndex = 57;
+            label14.Text = "Açıklama:";
+            // 
+            // rtxtAciklama
+            // 
+            rtxtAciklama.Location = new Point(496, 202);
+            rtxtAciklama.Name = "rtxtAciklama";
+            rtxtAciklama.Size = new Size(283, 126);
+            rtxtAciklama.TabIndex = 56;
+            rtxtAciklama.Text = "";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label13.Location = new Point(19, 362);
+            label13.Name = "label13";
+            label13.Size = new Size(36, 15);
+            label13.TabIndex = 55;
+            label13.Text = "Ülke:";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label12.Location = new Point(19, 333);
+            label12.Name = "label12";
+            label12.Size = new Size(30, 15);
+            label12.TabIndex = 54;
+            label12.Text = "İlçe:";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label11.Location = new Point(19, 304);
+            label11.Name = "label11";
+            label11.Size = new Size(17, 15);
+            label11.TabIndex = 53;
+            label11.Text = "İl:";
+            // 
+            // txtAdres
+            // 
+            txtAdres.Font = new Font("Segoe UI", 9F);
+            txtAdres.Location = new Point(154, 272);
+            txtAdres.Name = "txtAdres";
+            txtAdres.Size = new Size(258, 23);
+            txtAdres.TabIndex = 52;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label10.Location = new Point(19, 275);
+            label10.Name = "label10";
+            label10.Size = new Size(42, 15);
+            label10.TabIndex = 51;
+            label10.Text = "Adres:";
+            // 
+            // txtTelefon
+            // 
+            txtTelefon.Font = new Font("Segoe UI", 9F);
+            txtTelefon.Location = new Point(154, 243);
+            txtTelefon.Name = "txtTelefon";
+            txtTelefon.Size = new Size(258, 23);
+            txtTelefon.TabIndex = 50;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label9.Location = new Point(19, 246);
+            label9.Name = "label9";
+            label9.Size = new Size(52, 15);
+            label9.TabIndex = 49;
+            label9.Text = "Telefon:";
+            // 
+            // txtMail
+            // 
+            txtMail.Font = new Font("Segoe UI", 9F);
+            txtMail.Location = new Point(154, 214);
+            txtMail.Name = "txtMail";
+            txtMail.Size = new Size(258, 23);
+            txtMail.TabIndex = 48;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label8.Location = new Point(19, 217);
+            label8.Name = "label8";
+            label8.Size = new Size(44, 15);
+            label8.TabIndex = 47;
+            label8.Text = "E-mail:";
+            // 
+            // txtYonetici
+            // 
+            txtYonetici.Font = new Font("Segoe UI", 9F);
+            txtYonetici.Location = new Point(154, 185);
+            txtYonetici.Name = "txtYonetici";
+            txtYonetici.Size = new Size(258, 23);
+            txtYonetici.TabIndex = 46;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label7.Location = new Point(19, 188);
+            label7.Name = "label7";
+            label7.Size = new Size(127, 15);
+            label7.TabIndex = 45;
+            label7.Text = "Bağlı Olduğu Yönetici:";
+            // 
+            // txtDepartman
+            // 
+            txtDepartman.Font = new Font("Segoe UI", 9F);
+            txtDepartman.Location = new Point(154, 156);
+            txtDepartman.Name = "txtDepartman";
+            txtDepartman.Size = new Size(258, 23);
+            txtDepartman.TabIndex = 44;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label6.Location = new Point(19, 159);
+            label6.Name = "label6";
+            label6.Size = new Size(73, 15);
+            label6.TabIndex = 43;
+            label6.Text = "Departman:";
+            // 
+            // txtGorev
+            // 
+            txtGorev.Font = new Font("Segoe UI", 9F);
+            txtGorev.Location = new Point(154, 127);
+            txtGorev.Name = "txtGorev";
+            txtGorev.Size = new Size(258, 23);
+            txtGorev.TabIndex = 42;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label5.Location = new Point(19, 130);
+            label5.Name = "label5";
+            label5.Size = new Size(45, 15);
+            label5.TabIndex = 41;
+            label5.Text = "Görev:";
+            // 
+            // txtAdSoyad
+            // 
+            txtAdSoyad.Font = new Font("Segoe UI", 9F);
+            txtAdSoyad.Location = new Point(154, 41);
+            txtAdSoyad.Name = "txtAdSoyad";
+            txtAdSoyad.Size = new Size(258, 23);
+            txtAdSoyad.TabIndex = 40;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label4.Location = new Point(19, 101);
+            label4.Name = "label4";
+            label4.Size = new Size(50, 15);
+            label4.TabIndex = 39;
+            label4.Text = "Meslek:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label3.Location = new Point(19, 73);
+            label3.Name = "label3";
+            label3.Size = new Size(53, 15);
+            label3.TabIndex = 38;
+            label3.Text = "Cinsiyet:";
+            // 
+            // txtMeslek
+            // 
+            txtMeslek.Font = new Font("Segoe UI", 9F);
+            txtMeslek.Location = new Point(154, 98);
+            txtMeslek.Name = "txtMeslek";
+            txtMeslek.Size = new Size(258, 23);
+            txtMeslek.TabIndex = 37;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label2.Location = new Point(19, 44);
+            label2.Name = "label2";
+            label2.Size = new Size(63, 15);
+            label2.TabIndex = 36;
+            label2.Text = "Ad-Soyad:";
+            // 
+            // txtPlasiyerKodu
+            // 
+            txtPlasiyerKodu.Font = new Font("Segoe UI", 9F);
+            txtPlasiyerKodu.Location = new Point(154, 12);
+            txtPlasiyerKodu.Name = "txtPlasiyerKodu";
+            txtPlasiyerKodu.Size = new Size(258, 23);
+            txtPlasiyerKodu.TabIndex = 35;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label1.Location = new Point(19, 15);
+            label1.Name = "label1";
+            label1.Size = new Size(85, 15);
+            label1.TabIndex = 34;
+            label1.Text = "Müşteri Kodu:";
+            label1.Click += label1_Click;
+            // 
+            // rbKadin
+            // 
+            rbKadin.AutoSize = true;
+            rbKadin.Location = new Point(213, 71);
+            rbKadin.Name = "rbKadin";
+            rbKadin.Size = new Size(55, 19);
+            rbKadin.TabIndex = 65;
+            rbKadin.TabStop = true;
+            rbKadin.Text = "Kadın";
+            rbKadin.UseVisualStyleBackColor = true;
+            // 
+            // rbErkek
+            // 
+            rbErkek.AutoSize = true;
+            rbErkek.Location = new Point(154, 71);
+            rbErkek.Name = "rbErkek";
+            rbErkek.Size = new Size(53, 19);
+            rbErkek.TabIndex = 64;
+            rbErkek.TabStop = true;
+            rbErkek.Text = "Erkek";
+            rbErkek.UseVisualStyleBackColor = true;
+            // 
+            // MusteriKayit
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(800, 450);
+            Controls.Add(rbKadin);
+            Controls.Add(rbErkek);
+            Controls.Add(btnCikis);
+            Controls.Add(btnKaydet);
+            Controls.Add(cmbUlke);
+            Controls.Add(cmbIlce);
+            Controls.Add(cmbIl);
+            Controls.Add(label14);
+            Controls.Add(rtxtAciklama);
+            Controls.Add(label13);
+            Controls.Add(label12);
+            Controls.Add(label11);
+            Controls.Add(txtAdres);
+            Controls.Add(label10);
+            Controls.Add(txtTelefon);
+            Controls.Add(label9);
+            Controls.Add(txtMail);
+            Controls.Add(label8);
+            Controls.Add(txtYonetici);
+            Controls.Add(label7);
+            Controls.Add(txtDepartman);
+            Controls.Add(label6);
+            Controls.Add(txtGorev);
+            Controls.Add(label5);
+            Controls.Add(txtAdSoyad);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(txtMeslek);
+            Controls.Add(label2);
+            Controls.Add(txtPlasiyerKodu);
+            Controls.Add(label1);
+            Name = "MusteriKayit";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "MusteriKayit";
+            FormClosed += MusteriKayit_FormClosed;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnCikis;
+        private Button btnKaydet;
+        private ComboBox cmbUlke;
+        private ComboBox cmbIlce;
+        private ComboBox cmbIl;
+        private Label label14;
+        private RichTextBox rtxtAciklama;
+        private Label label13;
+        private Label label12;
+        private Label label11;
+        private TextBox txtAdres;
+        private Label label10;
+        private TextBox txtTelefon;
+        private Label label9;
+        private TextBox txtMail;
+        private Label label8;
+        private TextBox txtYonetici;
+        private Label label7;
+        private TextBox txtDepartman;
+        private Label label6;
+        private TextBox txtGorev;
+        private Label label5;
+        private TextBox txtAdSoyad;
+        private Label label4;
+        private Label label3;
+        private TextBox txtMeslek;
+        private Label label2;
+        private TextBox txtPlasiyerKodu;
+        private Label label1;
+        private RadioButton rbKadin;
+        private RadioButton rbErkek;
+    }
+}
---
+++ PlasiyerKayitPanel/SQLBaglantisi.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace C_Forms
+{
+    internal class SQLBaglantisi
+    {
+        public SqlConnection baglanti()
+        {
+            //SqlConnection baglan = new SqlConnection(@"Data Source=DIJITALOFIS1\NETSIS;Initial Catalog=KullaniciGirisi;Integrated Security=True");
+            SqlConnection baglan = new SqlConnection("Server=DIJITALOFIS1\\NETSIS;Database=NETSISTEST1;User Id=sa;Password=SA123pass_;");
+            baglan.Open();
+            return baglan;
+        }
+    }
+}
+
